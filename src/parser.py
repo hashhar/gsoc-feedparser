393b3cb450ee879b426bee0ac742e9cea373b871
Ashhar Hasan <hashhar_dev@outlook.com>
2016-07-01 04:01:21 +0530

    Date comparision working.
    Date comparision is working properly and the outline for the main function is completed with the exception of the decision about the log file (if we want one).
    
    sendmail function is to be written.

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import configparser
 import feedparser
+import smtplib
+import datetime
 
 config = configparser.ConfigParser()
 config.read('blogs.ini')
@@ -14,4 +16,18 @@ for blog in config.sections():
             date = d.entries[0].updated
     except:
         date = d.entires[0].published
-    print(config.get(blog, 'name') + ': ' + date)
+    # Get the activity_threshold.
+    expiry_days = config.get('DEFAULT', 'activity_threshold')
+    today_date = datetime.datetime.now()
+    # TODO: Is it okay to assume that the expiry_days will never be more than a month?
+    expiry_delta = datetime.timedelta(days=expiry_days)
+    # The date when the last post should have been done.
+    expired_date = today_date - expiry_delta
+    blog_date = datetime.datetime.strptime(date, "%a, %d %b %Y %I:%M:%S %Z")
+    # If the post is older than when we expected a new one send out emails.
+    if blog_date < expired_date:
+        # Call a function with the student email, mentor email and date when the last post was done.
+        sendmail(config.get(blog, 'owner_email'), config.get(blog, 'mentor_email'), date)
+        # TODO: Do we need a log file to track these events?
+    else:
+        # TODO: Do we also need to track good students? (Doesn't make sense at all, left here just in case)
